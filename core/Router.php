a84283a833f981206c3c28f6fa93d1582a5113eb
ismail <wibidjokam@gmail.com>
2022-03-12 12:57:16 +0700

    Update Response min 48

--- core/Router.php
+++ core/Router.php
@@ -5,11 +5,13 @@ namespace app\core;
 class Router
 {
     public Request $request;
+    public Response $response;
     protected array $routes = [];
 
-    public function __construct(\app\core\Request $request)
+    public function __construct(Request $request, Response $response)
     {
         $this->request = $request;
+        $this->response = $response;
     }
 
     public function get($path, $callback)
@@ -23,6 +25,7 @@ class Router
         $method = $this->request->getMethod();
         $callback = $this->routes[$method][$path] ?? false;
         if ($callback === false) {
+            $this->response->setStatusCode(404);
             return "Not found";
         }
         if (is_string($callback)){
