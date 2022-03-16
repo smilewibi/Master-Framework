799ce53e2b1b04a1cf872bdc369ed70917ce3eb6
ismail <wibidjokam@gmail.com>
2022-03-16 09:32:26 +0700

    Implement different layout

--- controllers/AuthController.php
+++ controllers/AuthController.php
@@ -8,6 +8,7 @@ class AuthController extends Controller
 {
     public function login()
     {
+        $this->setLayout('auth');
         return $this->render('login');
     }
 
@@ -16,6 +17,7 @@ class AuthController extends Controller
         if($request->isPost()){
             return 'Handle submitted data';
         }
+        $this->setLayout('auth');
         return $this->render('register');
     }
 }
--- core/Application.php
+++ core/Application.php
@@ -9,6 +9,7 @@ class Application
     public Request $request;
     public Response $response;
     public static Application $app;
+    public Controller $controller;
     public function __construct($rootPath)
     {
         self::$ROOT_DIR = $rootPath;
@@ -21,5 +22,15 @@ class Application
     public function run()
     {
         echo $this->router->resolve();
+    }
+
+    public function getController()
+    {
+        return $this->controller;
+    }
+
+    public function setController()
+    {
+        $this->controller = $controller;
     }
 }
